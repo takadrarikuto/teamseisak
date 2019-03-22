f3dd6b0142db5c446134d04682e0f154d67404e3
student <student@DEV2-25>
2019-03-22 16:38:54 +0900

    星図鑑に、戻るボタンを追加しました

--- star/主人公/GameHead.h
+++ star/主人公/GameHead.h
@@ -88,5 +88,5 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START CSceneTitle
+#define SET_GAME_START CSceneStarPicbook
 //-----------------------------------------------
