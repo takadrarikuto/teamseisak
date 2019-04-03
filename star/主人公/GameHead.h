d38069f1352cf7ca31e254a19e3f640bd61ebafb
student <student@DEV2-25>
2019-04-03 12:57:00 +0900

    消しました

--- star/主人公/GameHead.h
+++ star/主人公/GameHead.h
@@ -115,11 +115,5 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-//#define SET_GAME_START  CSceneMain
-//#define SET_GAME_START CSceneOperation
 #define SET_GAME_START CSceneTitle
-//#define SET_GAME_START CSceneGameKuria
-//#define SET_GAME_START CSceneStarPicbook
-//#define SET_GAME_START　CSceneSaturn
-//#define SET_GAME_START CSceneMars
 //-----------------------------------------------
