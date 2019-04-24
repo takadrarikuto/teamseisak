b297fdc3e576314be3aec58671a1edd7044d61f7
takadrarikuto <44048606+takadrarikuto@users.noreply.github.com>
2019-04-24 14:27:57 +0900

    魚座　羊座　作成

---
+++ star/主人公/ObjStarPresent4.h
@@ -0,0 +1,24 @@
+#pragma once
+//使用するヘッダーファイル
+#include "GameL\SceneObjManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：イベント
+class CObjStarPresent4 : public CObj
+{
+public:
+	CObjStarPresent4() {};	//コンストラクタ
+	~CObjStarPresent4() {};
+	void Init();	//イニシャライズ
+	void Action();	//アクション
+	void Draw();	//ドロー
+private:
+	float m_mou_x;//マウスの位置X
+	float m_mou_y;//マウスの位置X
+	bool m_mou_r;//マウスの右ボタン
+	bool m_mou_l;//マウスの左ボタン
+
+	wchar_t str[256];
+};
