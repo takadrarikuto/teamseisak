8c9df2c540f93d7ec0e104bcdd246d53c080b290
takadrarikuto <44048606+takadrarikuto@users.noreply.github.com>
2019-04-26 13:26:23 +0900

    Add files via upload

---
+++ star/主人公/ObjStarPresent8.h
@@ -0,0 +1,24 @@
+#pragma once
+//使用するヘッダーファイル
+#include "GameL\SceneObjManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：イベント
+class CObjStarPresent8 : public CObj
+{
+public:
+	CObjStarPresent8() {};	//コンストラクタ
+	~CObjStarPresent8() {};
+	void Init();	//イニシャライズ
+	void Action();	//アクション
+	void Draw();	//ドロー
+private:
+	float m_mou_x;//マウスの位置X
+	float m_mou_y;//マウスの位置X
+	bool m_mou_r;//マウスの右ボタン
+	bool m_mou_l;//マウスの左ボタン
+
+	wchar_t str[256];
+};
