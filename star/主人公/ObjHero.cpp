986dd06e3d3d3e4cc0f9055da3436c3bf21d09da
takaderarikuto <h1201819020@hiratagakuen.onmicrosoft.com>
2019-05-08 11:44:17 +0900

    酸素回復　更新

--- star/主人公/ObjHero.cpp
+++ star/主人公/ObjHero.cpp
@@ -12,6 +12,11 @@
 //使用するネームスペース
 using namespace GameL;
 
+extern bool star_flag;
+extern int FiStar_Reco;
+extern int SeStar_Reco;
+extern int ThStar_Reco;
+
 //イニシャライズ
 void CObjHero::Init()
 {
@@ -125,6 +130,26 @@ void CObjHero::Action()
 
 	//自身のHitBoxを持ってくる
 	CHitBox* hit = Hits::GetHitBox(this);
+
+	if (hit->CheckObjNameHit(OBJ_FIRSTSTAR) != nullptr || hit->CheckObjNameHit(OBJ_SECONDSTAR) != nullptr
+		|| hit->CheckObjNameHit(OBJ_THIRDSTAR) != nullptr || hit->CheckObjNameHit(OBJ_FOURTHSTAR) != nullptr
+		|| hit->CheckObjNameHit(OBJ_OTHERSTAR) != nullptr)
+	{
+		star_flag = true;
+		if (hit->CheckObjNameHit(OBJ_FIRSTSTAR) != nullptr)
+		{
+			FiStar_Reco += 1;
+		}
+		else if (hit->CheckObjNameHit(OBJ_SECONDSTAR) != nullptr)
+		{
+			SeStar_Reco += 1;
+		}
+		else if (hit->CheckObjNameHit(OBJ_THIRDSTAR) != nullptr)
+		{
+			ThStar_Reco += 1;
+		}
+	}
+
 
 	//HitBoxの位置の変更
 	hit->SetPos(m_px, m_py);
