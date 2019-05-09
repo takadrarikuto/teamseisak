8b01a2551728ae361cac0dd4c746fd3d9ef51021
takadrarikuto <44048606+takadrarikuto@users.noreply.github.com>
2019-05-09 15:33:50 +0900

    Add files via upload

---
+++ star/主人公/ObjStarPresent4.h
@@ -0,0 +1,19 @@
+#pragma once
+//使用するヘッダーファイル
+#include "GameL\SceneObjManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：イベント
+class CObjStarPresent4 : public CObjStarPresent
+{
+public:
+	CObjStarPresent4() {};	//コンストラクタ
+	~CObjStarPresent4() {};
+	void Init();	//イニシャライズ
+	void Action();	//アクション
+	void Draw();	//ドロー
+private:
+
+};
