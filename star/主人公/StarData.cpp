ba3a9b7dda75d63141331ca3bf1c1903199c7cd7
hamanakaR <48446334+hamanakaR@users.noreply.github.com>
2019-05-23 10:11:05 +0900

    Add files via upload

--- star/主人公/StarData.cpp
+++ star/主人公/StarData.cpp
@@ -1,7 +1,47 @@
-int g_first_star[5] = {};//1等星：5種
-int g_second_star[17] = {};//二等星：17種
-int g_third_star[29] = {};//三等星：29種
-int g_other_star[15] = {};//5等星：13種＋6等星：2種
+#include"GameL\SceneObjManager.h"
+#include"GameL\DrawTexture.h"
+#include"GameL\DrawFont.h"
+#include"GameL\Audio.h"
+#include"GameL\UserData.h"
+#include"GameHead.h"
+
+int g_Aries[5] = {};//牡羊座：6種
+int g_Taurus[17] = {};//牡牛座：16種
+int g_Gemini[29] = {};//双子座：10種
+int g_Cancer[5] = {};//蟹座：9種
+int g_Leo[17] = {};//獅子座：9種
+int g_Virgo[29] = {};//乙女座：10種
+int g_Libra[5] = {};//天秤座：4種
+int g_Scorpio[17] = {};//蠍座：15種
+int g_Sagittarius[29] = {};//射手座：13種
+int g_Capricorn[5] = {};//山羊座：5種
+int g_Aquarius[17] = {};//水瓶座：7種
+int g_Pisces[29] = {};//魚座：4種
+
 int g_debris;//はずれ枠
 
 int star_count;//星総数カウント用
+
+unique_ptr<wchar_t>p;//ステージ情報ポインター
+int size;			//ステージ情報の大きさ
+p = Save::ExternalDataOpen(L"Book1.csv", &size);//外部データ読み込み
+
+int map[56][56];
+int count = 1;
+for (int i = 0; i < 56; i++)
+{
+	for (int j = 0; j < 56; j++)
+	{
+		int w = 0;
+		swscanf_s(&p.get()[count], L"%d", &w);
+
+		map[i][j] = w;
+
+		if (w >= 10)
+		{
+			count += 1;
+		}
+
+		count += 2;
+	}
+}
